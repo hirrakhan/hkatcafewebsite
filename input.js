1957af73b4b6b983f58bcf8848aba54db372364a
hirrakhan <79573119+hirrakhan@users.noreply.github.com>
2021-05-16 10:20:52 +0100

    Add files via upload

---
+++ input.js
@@ -0,0 +1,8 @@
+console.log(firstname.value);		//"firstname"
+console.log(lastname.value);		//"lastname"
+console.log(radio2.checked);
+console.log(radio4.checked);
+console.log(radio6.checked);
+console.log(email.value);
+console.log(date.value);
+console.log(time.value);
