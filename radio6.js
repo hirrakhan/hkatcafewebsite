e7ab2522d8889b760f4887da476e04ff81e2f45d
hirrakhan <79573119+hirrakhan@users.noreply.github.com>
2021-05-16 19:40:11 +0100

    Update radio6.js

--- radio6.js
+++ radio6.js
@@ -1,2 +1,12 @@
+
+# About Author
+- **Author:** *Hirra Khan*					
+- **SCCB S-Number:** *S45139003*
+- **DMU P-Number:** *P2652223*
+- **SCCB Assessor:** *Aram Saeed*			
+- **DMU Module:** *CTEC3905 Front-End Web Development*
+
+```
 const x = (x, y) => { return x * y };
 document.getElementById("cal6").innerHTML = x(2, 3);
+```
